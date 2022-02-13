61228ce1f361bbe96e2d92a6a8aaa17da0dd5cc2
AnaLucia Rojas <analuciarojas@AnaLucias-MacBook-Pro.local>
2022-02-13 09:44:34 -0800

    fixed sintax errors on seed file and fixed database and view employee, department, and role function

--- db/seed.sql
+++ db/seed.sql
@@ -3,34 +3,36 @@ USE employee_DB;
 INSERT INTO department (name)
 VALUES ("Engineering"),
         ("Finance"),
-        ("Marketing");
+        ("Marketing"),
         ("Sales"),
         ("Legal");
 
-SELECT * FROM DEPARTMENT;
+SELECT * FROM department;
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("Software Engineer", 120000, 1);
+VALUES ("Software Engineer", 120000, 1),
         ("Project Manager", 140000, 1),
-        ("Engineering Manager", 160000, 1),
         ("Lead Engineer", 150000, 1),
         ("Accountant", 125000, 2),
         ("Account Manager", 160000, 2),
         ("Marketing Manager", 50000, 3),
         ("Marketing Lead", 140000, 3),
-        ("Sales Rep", 85000, 4);
-        ("Salesperson", 80000, 4);
-        ("Legal Team Lead", 250000, 5);
+        ("Sales Rep", 85000, 4),
+        ("Salesperson", 80000, 4),
+        ("Legal Team Lead", 250000, 5),
         ("Lawyer", 190000, 5);
 
-SELECT * FROM ROLE;
+SELECT * FROM role;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Mike", "Chan", 2, NULL),
+VALUES ("Mike", "Chan", 9, NULL),
     ("Ashley", "Rodriguez", 3, NULL),
-    ("Kevin", "Tupik", 1, 3),
-    ("Kunal", "Singh", 2, NULL),
-    ("Malia", "Brown", 3, 6);
+    ("Kevin", "Tupik", 1, 2),
+    ("Kunal", "Singh", 5, NULL),
+    ("Malia", "Brown", 4, 4),
+    ("Sarah", "Lourd", 10, NULL),
+    ("Tom", "Allen", 11, 6);
+
 
 SELECT * FROM employee;
 
--- server.js
+++ server.js
@@ -120,21 +120,19 @@ const viewAll = (table) => {
       query = `SELECT * FROM DEPARTMENT`;
     } else if (table === "roles") {
         console.log("VIEWING ROLES\n");
+        query = `SELECT * FROM ROLE`;
 
-      query = `SELECT R.id AS id, title, salary, D.name AS department
-      FROM ROLE AS R LEFT JOIN DEPARTMENT AS D
-      ON R.department_id = D.id;`;
     } else {// Default employees
         console.log("VIEWING EMPLOYEES\n");
 
       query = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
-  FROM employee e
-  LEFT JOIN role r
-	ON e.role_id = r.id
-  LEFT JOIN department d
-  ON d.id = r.department_id
-  LEFT JOIN employee m
-	ON m.id = e.manager_id`;
+      FROM employee e
+      LEFT JOIN role r
+        ON e.role_id = r.id
+      LEFT JOIN department d
+      ON d.id = r.department_id
+      LEFT JOIN employee m
+        ON m.id = e.manager_id`;
   
     }
     connection.query(query, (err, res) => {
